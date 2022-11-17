860951c280980689c0d58837c279584b0b35fb7a
Francisco Javier García Márquez <javiergarciamarquez@icloud.com>
2022-11-17 20:02:24 +0100

    Add el adaptador de vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	},
-	preprocess: [
-		preprocess({
-		  postcss: true,
-		}),
-	  ]
+		// default options are shown
+		adapter: vercel({
+		  // if true, will deploy the app using edge functions
+		  // (https://vercel.com/docs/concepts/functions/edge-functions)
+		  // rather than serverless functions
+		  edge: false,
+	
+		  // an array of dependencies that esbuild should treat
+		  // as external when bundling functions
+		  external: [],
+	
+		  // if true, will split your app into multiple functions
+		  // instead of creating a single one for the entire app
+		  split: false
+		})
+	  }
 };
 
 export default config;
