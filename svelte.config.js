3234b09fc5063b87343f5297079721e9f0877890
Francisco Javier García Márquez <javiergarciamarquez@icloud.com>
2022-11-18 09:14:13 +0100

    Vuelta al adaptador universal. Lo porbare en Heroku

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import node from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: node({ env: { port: process|.env.PORT } }),
+		adapter: adapter(),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
